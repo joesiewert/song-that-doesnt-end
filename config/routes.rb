9e26a593a1ca7f3dc75514a027d83d6c220ffcab
joesiewert <joe.siewert@gmail.com>
2014-10-14 10:51:20 -0600

    Completed exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get 'verse-2' => 'verse#verse2', as: :verse2
+  get 'verse-3' => 'verse#verse3', as: :verse3
+  get 'verse-4' => 'verse#verse4', as: :verse4
 end
